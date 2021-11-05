58106157d0c6f66717787d25f6b35a08c9edaeb9
Luiz Flavio <luizflaviotb@gmail.com>
2021-11-04 22:53:12 -0400

    readme update and course information

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# html-basico
+# Curso HTML Básico - School Of Net 
+
+Aplicação hospedada no Heroku [visualizar projeto](https://html-basico.herokuapp.com/)
---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Luiz Flavio - HTML Básico</title>
+
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+  <div class="container">
+    <h1>Curso HTML Básico</h1>
+    <p class="description">
+      Curso de HTML Básico Ministrado pelo Professor <br> Luis Carlos 
+      na plataforma
+      <a href="https://www.schoolofnet.com/" target="_blank"> School of Net.</a>  
+    </p>
+    <p class="link">
+      <a href="./01-ola-mundo.html" target="_blank">Aula 01 - Olá mundo</a>
+    </p>
+    <p class="link">
+      <a href="./02-paragrafos.html" target="_blank">Aula 02 - Parágrafos</a>
+    </p>
+    <p class="link">
+      <a href="./03-titulos.html" target="_blank">Aula 03 - Títulos</a>
+    </p>
+    <p class="link">
+      <a href="./04-centralizacao-e-quebra-linha.html" target="_blank">Aula 04 - Centralizacao e quebra linha</a>
+    </p>
+    <p class="link">
+      <a href="./05-listas.html" target="_blank">Aula 05 - Listas</a>
+    </p>
+    <p class="link">
+      <a href="./06-tabelas.html" target="_blank">Aula 06 - Tabelas</a>
+    </p>
+    <p class="link">
+      <a href="./07-estilizacao-e-cores.html" target="_blank">Aula 07 - Estilização e cores</a>
+    </p>
+    <p class="link">
+      <a href="./08-links.html" target="_blank">Aula 08 - Links</a>
+    </p>
+    <p class="link">
+      <a href="./09-navegacao.html" target="_blank">Aula 09 - Navegação</a>
+    </p>
+    <p class="link">
+      <a href="./10-imagens.html" target="_blank">Aula 10 - Imagens</a>
+    </p>
+    <p class="link">
+      <a href="./11-formularios.html" target="_blank">Aula 11 - Formulários</a>
+    </p>
+    <p class="link">
+      <a href="./12-formularios-pesquisa-google.html" target="_blank">Aula 12 - Formulários de pesquisa </a>
+    </p>
+  </div>
+</body>
+
+</html>
